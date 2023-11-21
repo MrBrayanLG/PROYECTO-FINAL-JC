9574c5f7ba43b5cd48c3ae2d1b025d04db4bd67b
Brayan <legardabrayan40@gmail.com>
2023-11-20 23:59:50 -0500

    se agregó cambio reseña reseña terminado

--- Index.html/inicio.js
+++ Index.html/inicio.js
@@ -21,3 +21,50 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+
+
+
+
+
+
+window.onload = function () {
+    cargarReseñas();
+};
+
+function agregarReseña() {
+    var nombre = document.getElementById('nombre').value;
+    var comentario = document.getElementById('reseña').value;
+
+    if (nombre && comentario) {
+        var nuevaReseña = document.createElement('div');
+        nuevaReseña.innerHTML = '<div class="resena"><h3 class="cliente-nombre">' + nombre + '</h3><p class="cliente-resena">' + comentario + '</p></div>';
+
+        document.getElementById('resenas-container').appendChild(nuevaReseña);
+
+        document.getElementById('nombre').value = '';
+        document.getElementById('reseña').value = '';
+
+        guardarReseña(nombre, comentario);
+    } else {
+        alert('Por favor, completa ambos campos antes de enviar la reseña.');
+    }
+}
+
+function guardarReseña(nombre, comentario) {
+    var reseñasGuardadas = JSON.parse(localStorage.getItem('reseñas')) || [];
+
+    reseñasGuardadas.push({ nombre: nombre, comentario: comentario });
+
+    localStorage.setItem('reseñas', JSON.stringify(reseñasGuardadas));
+}
+
+function cargarReseñas() {
+    var reseñasGuardadas = JSON.parse(localStorage.getItem('reseñas')) || [];
+
+    var contenedorReseñas = document.getElementById('resenas-container');
+    reseñasGuardadas.forEach(function (reseña) {
+        var nuevaReseña = document.createElement('div');
+        nuevaReseña.innerHTML = '<div class="resena"><h3 class="cliente-nombre">' + reseña.nombre + '</h3><p class="cliente-resena">' + reseña.comentario + '</p></div>';
+        contenedorReseñas.appendChild(nuevaReseña);
+    });
+}
